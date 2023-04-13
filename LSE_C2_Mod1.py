7ed976005151d7bb266133416da05f2f8dd72ef3
Angus Ratcliffe <118676489+AngusRatski@users.noreply.github.com>
2023-04-13 10:53:59 +0100

    My first python code

---
+++ LSE_C2_Mod1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#this is the first line of code
+print("My name is Angus")
+print("The date is Thursday 13th April 2023")
+
+
+# In[2]:
+
+
+#write a program to analyse real estate data
+#create variables
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+
+# In[4]:
+
+
+#find total sales values
+total_sales = a + b + c + d + e
+print(total_sales)
+
+
+# In[5]:
+
+
+#calculate average sales price for the month
+average_sales = total_sales/5
+print(average_sales)
+
+
+# In[6]:
+
+
+#find the missing property price value
+print(c-6500)
+
+
+# In[ ]:
+
+
+
+
